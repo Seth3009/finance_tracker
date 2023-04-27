65d850c4ce898c0c1bbc9ad1eb840e587a51d5b8
Setiadi <setiadi.seth@gmail.com>
2023-04-27 14:23:00 +0700

    add many t- many relationships between user

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -6,6 +10,13 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex_client[:secret_key],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.quote(ticker_symbol).latest_price
+
+    begin
+      new(ticker: ticker_symbol, 
+          name: client.company(ticker_symbol).company_name, 
+          last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
   end
 end
