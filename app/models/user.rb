5843f4f2ca35476025dd61461fb0e0857a2bd869
Setiadi <setiadi.seth@gmail.com>
2023-05-23 15:21:10 +0700

    finalize the functional tracker app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -23,5 +25,18 @@ class User < ApplicationRecord
   def full_name
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymous"
+  end
+
+  def self.search(param)
+    param.strip!
+    where("first_name LIKE :param OR last_name LIKE :param OR email LIKE :param", param: "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user == self }
+  end
+
+  def not_friend?(friend)
+    self.friends.exclude?(friend)
   end
 end
